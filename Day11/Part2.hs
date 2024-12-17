b941b8ce314d7e49ddad9e6134e209232e654302
olligobber <sirolligobbervii@yahoo.com.au>
2024-12-17 23:32:33 +1100

    Made cache syntax nicer

--- Day11/Part2.hs
+++ Day11/Part2.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+
 import Data.Map (Map)
 import qualified Data.Map as M
 import Control.Monad.State.Lazy (State)
@@ -12,6 +14,12 @@ type Cached x = State Cache x
 
 runCached :: Cached x -> x
 runCached s = S.evalState s emptyCache
+
+getCache :: Int -> Stone -> Cached (Maybe Integer)
+getCache n x = S.gets (M.!? (n,x))
+
+putCache :: Int -> Stone -> Integer -> Cached ()
+putCache n x y = S.modify $ M.insert (n,x) y
 
 type Stone = Integer
 
@@ -28,15 +36,13 @@ blink n = [2024 * n]
 
 cachedLength :: Int -> Stone -> Cached Integer
 cachedLength 0 _ = pure 1
-cachedLength n x = do
-	fromCache <- S.gets (M.!? (n,x))
-	case fromCache of
-		Nothing -> do
-			let ys = blink x
-			y <- sum <$> traverse (cachedLength $ n-1) ys
-			S.modify $ M.insert (n, x) y
-			pure y
-		Just y -> pure y
+cachedLength n x = getCache n x >>= \case
+	Nothing -> do
+		let ys = blink x
+		y <- sum <$> traverse (cachedLength $ n-1) ys
+		putCache n x y
+		pure y
+	Just y -> pure y
 
 getInput :: IO String
 getInput = readFile "input"
